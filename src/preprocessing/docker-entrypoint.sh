05215c9ce446f05a5301f495adc21fb000a614d4
Sam Crowder <scrowder@groq.com>
2024-10-17 00:52:54 -0400

    copy data into repo and bootstrap preprocess container / script

---
+++ src/preprocessing/docker-entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Starting script"
+exec python preprocess.py "$@"
---
+++ src/preprocessing/docker-shell.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# exit immediately if a command exits with a non-zero status
+set -e
+
+export IMAGE_NAME="preprocessing-preprocess-image"
+export BASE_DIR=$(pwd)
+export SECRETS_DIR=$(pwd)/../../../secrets/
+export GCS_BUCKET_NAME="msmballstars_data"
+export GCP_PROJECT="AC215"
+export GCP_ZONE="us-central1-a"
+export GOOGLE_APPLICATION_CREDENTIALS=/../secrets/data-service-account.json
+
+# Check to see if path to secrets is correct
+if [ ! -f "$SECRETS_DIR/data-service-account.json" ]; then
+    echo "data-service-account.json not found at the path you have provided."
+    exit 1
+fi
+
+# Build the image based on the Dockerfile
+docker build -t $IMAGE_NAME -f Dockerfile .
+
+echo "Host GOOGLE_APPLICATION_CREDENTIALS: $GOOGLE_APPLICATION_CREDENTIALS"
+
+# Check if system has NVIDIA GPUs
+if nvidia-smi > /dev/null 2>&1; then
+    GPU_FLAGS="--gpus all"
+else
+    GPU_FLAGS=""
+fi
+
+# Run the container
+# Run Docker with an initial command to check for the secret before proceeding
+docker run --rm --name $IMAGE_NAME -i $GPU_FLAGS \
+--mount type=bind,source="$BASE_DIR",target=/app \
+--mount type=bind,source="$SECRETS_DIR",target=/secrets \
+-e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
+-e GCP_PROJECT="$GCP_PROJECT" \
+-e GCP_ZONE=$GCP_ZONE \
+-e GCS_BUCKET_NAME=$GCS_BUCKET_NAME \
+-e DEV=1 $IMAGE_NAME /bin/bash -c
