3fa5e1c5fd69469f3f691a4a040046f79e8a5d3c
Sam Crowder <scrowder@groq.com>
2024-11-19 09:06:49 -0500

    frontend unit tests pass

--- src/frontend/tennis-app/src/App.test.tsx
+++ src/frontend/tennis-app/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Game-Set-Match', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Game-Set-Match/i);
   expect(linkElement).toBeInTheDocument();
 });
