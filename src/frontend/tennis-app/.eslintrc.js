d7b84dedfa84acd48bcb51d906bae0bd40ad9fbf
Sam Crowder <scrowder@groq.com>
2024-11-19 13:42:07 -0500

    add command to run eslint

---
+++ src/frontend/tennis-app/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: ['react-app', 'react-app/jest'],
+    rules: {
+      'no-console': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'react/prop-types': 'off'
+    }
+  };
