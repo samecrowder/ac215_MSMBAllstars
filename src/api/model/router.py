e8e723ae5627ed3784d94cee64877a77fa359e53
Sam Crowder <scrowder@groq.com>
2024-11-18 22:30:28 -0500

    change API of the predict endpoint

--- src/api/model/router.py
+++ src/api/model/router.py
@@ -1,14 +1,26 @@
 # model routes
 
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 from ..external.model_service import get_victory_prediction
 
 router = APIRouter()
 
 
-@router.post("/predict")
-def predict(player_a_id: str, player_b_id: str, lookback: int = 10):
-    return {
-        "player_a_win_probability": get_victory_prediction(player_a_id, player_b_id, lookback)
-    }
+class PredictionRequest(BaseModel):
+    player_a_id: str
+    player_b_id: str
+    lookback: int = 10  # Default value if not provided
+
+
+class PredictionResponse(BaseModel):
+    player_a_win_probability: float
+
+
+@router.post("/predict", response_model=PredictionResponse)
+def predict(request: PredictionRequest):
+    probability = get_victory_prediction(
+        request.player_a_id, request.player_b_id, request.lookback
+    )
+    return PredictionResponse(player_a_win_probability=probability)
