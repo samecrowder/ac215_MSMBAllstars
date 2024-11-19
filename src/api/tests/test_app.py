91c1ef42eae6b6769e566647a85d4ce4ac5b21d7
Sam Crowder <scrowder@groq.com>
2024-11-19 14:16:14 -0500

    set env is test in the pipeline file

--- src/api/tests/test_app.py
+++ src/api/tests/test_app.py
@@ -1,13 +1,10 @@
 import sys
 import os
-
 from fastapi.testclient import TestClient
 from app import app
 
-
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-os.environ["ENV"] = "test"  # Set test environment
 client = TestClient(app)
 
 
