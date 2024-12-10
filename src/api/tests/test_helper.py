ddf0082a2088c15f2b0f7491ec8778872adf1f20
“kennethschen” <kennychen12@gmail.com>
2024-12-10 15:32:58 -0500

    black

--- src/api/tests/test_helper.py
+++ src/api/tests/test_helper.py
@@ -13,7 +13,7 @@ from external.helper import (  # noqa: E402
     get_player_last_nplus1_matches,
     get_player_last_nplus1_matches_since_date,
     get_h2h_match_history,
-    get_h2h_match_history_since_date
+    get_h2h_match_history_since_date,
 )
 
 
