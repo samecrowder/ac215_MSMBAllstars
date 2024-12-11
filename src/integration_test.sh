ef3bfc1d780450f1381e43e0a3d512bae56e1665
“kennethschen” <kennychen12@gmail.com>
2024-12-10 20:30:19 -0500

    increase waits

--- src/integration_test.sh
+++ src/integration_test.sh
@@ -35,7 +35,7 @@ elapsed=0
     status_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8000/health || echo "failed")
         
         if [ "$status_code" = "200" ]; then
-        sleep 10 # wait for 10 seconds for other services to be ready
+        sleep 60 # wait for other services to be ready
         echo "✅ API is ready!"
             break
         fi
