3618d2741edb66f47223e3d149643e03cb56c80b
“kennethschen” <kennychen12@gmail.com>
2024-12-10 19:59:56 -0500

    wait to fix race condition in integration test

--- src/probability_model/app.py
+++ src/probability_model/app.py
@@ -12,7 +12,7 @@ from pydantic import BaseModel
 from sklearn.preprocessing import StandardScaler
 
 if os.environ.get("ENV") != "test":
-    from ac215_MSMBAllstars.src.probability_model.model import TennisLSTM
+    from .model import TennisLSTM
 else:
     # Mock TennisLSTM for non-prod environments
     class TennisLSTM:
