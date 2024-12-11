7c5ccf54870203c50486fc2a851758124ed71b4e
“kennethschen” <kennychen12@gmail.com>
2024-12-10 20:18:41 -0500

    revert test app

--- src/probability_model/tests/test_app.py
+++ src/probability_model/tests/test_app.py
@@ -7,7 +7,6 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 def test_import_app():
     """Just try to import app.py"""
-    os.environ["ENV"] = "test"  # Set test environment
-    from probability_model import app  # Use absolute import
+    import app
 
     assert app is not None
